cdab1aa0cb906a9a3b0b730f56cca742b9cc276b
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2022-10-26 22:57:43 +0200

    added routes

--- server/server.js
+++ server/server.js
@@ -1,18 +1,21 @@
-const express = require('express')
-const app = express()
-const PORT = 8080
-const dotenv = require('dotenv')
-dotenv.config()
+const express = require("express");
+const app = express();
+const PORT = 8080;
+const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+const usersRoute = require("./routes/users");
+const bookedTimesRoute = require("./routes/bookedtimes");
+dotenv.config();
 
-app.use(express.json())
+app.use(express.json());
+app.use("/auth", authRoute);
+app.use("/users", usersRoute);
+app.use("/booked", bookedTimesRoute);
 
+app.get("/", (req, res) => {
+  res.send("HELO WORLD");
+});
 
-
-app.get('/',(req,res)=>{ 
-    res.send('HELO WORLD')
-})
-
-
-app.listen(PORT, ()=>{
-    console.log(`server listening on port: ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`server listening on port: ${PORT}`);
+});
