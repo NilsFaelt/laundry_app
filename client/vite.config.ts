42a754cccb043b6161647a9a9ada6ef374e9738f
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2022-11-29 15:08:00 +0100

    trying to fix pwa

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,63 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// import { VitePWA } from "vite-plugin-pwa";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     VitePWA({
+//       registerType: "prompt",
+//       includeAssets: ["logo.png"],
+//       manifest: {
+//         name: "Laundry App Pwa",
+//         short_name: "laundry app",
+//         description: "Laundry booking app)",
+//         theme_color: "#ffffff",
+//         start_url: "/",
+//         icons: [
+//           {
+//             src: "logo.png",
+//             sizes: "10242x1024",
+//             type: "image/png",
+//           },
+//         ],
+//       },
+//     }),
+//   ],
+// });
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "prompt",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.png"],
+      manifest: {
+        name: "laundry app pwa",
+        short_name: "laundry app",
+        description: "a laundr booking app)",
+        theme_color: "#ffffff",
+        start_url: ".",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
 });
-
-// VitePWA()
